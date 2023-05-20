b92e3d1a03d600faef1208b92decc257b84cdce1
Pride30 <eliott@famillefavier.fr>
2023-05-20 21:16:30 +0200

    Forme global

--- Map.py
+++ Map.py
@@ -1,3 +1,5 @@
+## Importations
+
 import time
 
 import numpy
@@ -13,8 +15,46 @@ from perlin1DEliott import *
 
 from nbtschematic import SchematicFile
 
+## Carte
 
-## Fonctions prédéfinies
+def Patron_carte(BruitP2D):
+    eau = []
+    sable = []
+    CarteListe3D = [[[1 for y in range(Taille)] for z in range(hauteur)] for x in trange(Taille)]
+
+    for x in trange(Taille):
+        for y in range(Taille):
+            Hsurface = int(BruitP2D[x, y])
+            z = 0
+            while z < Hsurface:
+                CarteListe3D[x][z][y] = 0
+                if z >= Heau:
+                    eau.append((x, y, z))
+                z += 1
+            if z < Hneige:
+                CarteListe3D[x][z][y] = 11
+            elif z < Heau - 4:
+                CarteListe3D[x][z][y] = 2
+            else:
+                CarteListe3D[x][z][y] = 5
+                if z >= Heau:
+                    sable.append((x, y, z))
+            z += 1
+            a = 3
+            for i in range(a):
+                if z + i >= Heau - 4:
+                    CarteListe3D[x][z + i][y] = 5
+                else:
+                    CarteListe3D[x][z + i][y] = 3
+            if random.random() < 1 / 3:
+                CarteListe3D[x][hauteur - 3][y] = 10
+            if random.random() < 1 / 2:
+                CarteListe3D[x][hauteur - 2][y] = 10
+            CarteListe3D[x][hauteur - 1][y] = 10
+
+    return CarteListe3D, sable, eau
+
+## Images
 def sauvegarder_grille(grille: list, g, i, nom_de_fichier: str) -> None:
     echelle = ListedColormap(['white', 'gray', 'green', 'brown', 'blue', 'yellow', 'black', 'red', 'aqua', 'gold', '0.3', '0.8','beige'], 12)
     plt.matshow(grille, cmap=echelle, vmin=0, vmax=12)
@@ -23,8 +63,19 @@ def sauvegarder_grille(grille: list, g, i, nom_de_fichier: str) -> None:
     plt.ylabel('z')
     plt.savefig(nom_de_fichier)
 
+def frames(CarteListe3D):
+    for i in trange(Taille):
+        sauvegarder_grille(CarteListe3D[i], graine, i, f"Affichage_de_la_map/Frame/TIPE{i}.jpg")
+        plt.close()
 
-## Eléments suplémentaire sur la carte
+def gif():
+    frames = []
+    for i in trange(Taille):
+        image = imageio.v2.imread(f"Affichage_de_la_map/Frame/TIPE{i}.jpg")
+        frames.append(image)
+    imageio.mimsave(f"Affichage_de_la_map/GIF.gif", frames, duration=7)
+
+## Minerais
 
 def diamant(C, x, z, y):
     for j in range(1):
@@ -61,7 +112,63 @@ def gold(C, x, z, y):
                 if C[x+i][z+k][y+j]==1 and random.random()<0.5:
                     C[x+i][z+k][y+j] = 9
 
-def explose(C, x, z, y):  # fait disparaitre les blocs autour du bloc de coordonnées
+def minerais(CarteListe3D, ax):
+    h = ((Taille**2)/2500)
+
+    # charbon
+    k = int(15*h)
+    _, Yc = liste_aleatoire(k, Taille - 4, k)
+    _, Xc = liste_aleatoire(k, Taille - 4, k)
+    _, Z = liste_aleatoire(k, hauteur // 2 - 4, k)
+    Zc = [i + hauteur // 2 for i in Z]
+    for i in range(k):
+        charbon(CarteListe3D, int(Xc[i]), int(Zc[i]), int(Yc[i]))
+
+    # redstone
+    k = int(11*h)
+    _, Yr = liste_aleatoire(k, Taille - 4, k)
+    _, Xr = liste_aleatoire(k, Taille - 4, k)
+    _, Z = liste_aleatoire(k, hauteur // 4 - 4, k)
+    Zr = [i + 3*(hauteur // 4) for i in Z]
+    for i in range(k):
+        redstone(CarteListe3D, int(Xr[i]), int(Zr[i]), int(Yr[i]))
+
+    # fer
+    k = int(11*h)
+    _, Yi = liste_aleatoire(k, Taille - 3, k)
+    _, Xi = liste_aleatoire(k, Taille - 3, k)
+    _, Z = liste_aleatoire(k, hauteur // 3 - 3, k)
+    Zi = [i + 2*(hauteur // 3) for i in Z]
+    for i in range(k):
+        iron(CarteListe3D, int(Xi[i]), int(Zi[i]), int(Yi[i]))
+
+    # or
+    k = int(8*h)
+    _, Yo = liste_aleatoire(k, Taille - 2, k)
+    _, Xo = liste_aleatoire(k, Taille - 2, k)
+    _, Z = liste_aleatoire(k, hauteur // 2 - 2, k)
+    Zo = [i + hauteur // 2 for i in Z]
+    for i in range(k):
+        gold(CarteListe3D, int(Xo[i]), int(Zo[i]), int(Yo[i]))
+
+    # diamant
+    k = int(5*h)
+    _, Yd = liste_aleatoire(k, Taille - 2, k)
+    _, Xd = liste_aleatoire(k, Taille - 2, k)
+    _, Z = liste_aleatoire(k, hauteur // 5 - 2, k)
+    Zd = [i + 4 * (hauteur // 5) for i in Z]
+    for i in range(k):
+        diamant(CarteListe3D, int(Xd[i]), int(Zd[i]), int(Yd[i]))
+
+    ax.scatter(Xr, Yr, Zr, c='red', s=0.7)
+    ax.scatter(Xi, Yi, Zi, c='beige', s=0.7)
+    ax.scatter(Xo, Yo, Zo, c='gold', s=0.7)
+    ax.scatter(Xc, Yc, Zc, c='black', s=0.7)
+    ax.scatter(Xd, Yd, Zd, c='aqua', s=0.7)
+
+## Grottes
+
+def explose(C, x, z, y):
 
     # regarde si le bloc n'est pas trop près des bords
 
@@ -165,6 +272,45 @@ def explose(C, x, z, y):  # fait disparaitre les blocs autour du bloc de coordon
             C[x - 1][z - 1][y - 2] = 0
             C[x][z - 1][y - 2] = 0
 
+def grotte(CarteListe3D, ax):
+    u = 5
+    _, Xg = liste_aleatoire_spline1(Taille, Taille, u, alpha)
+    _, Zg = liste_aleatoire_spline1(Taille, hauteur, u, alpha)
+    _, Yg = liste_aleatoire_spline1(Taille, Taille, u, alpha)
+
+    '''
+    plt.close("all")
+    plt.plot(Ag, Zg, 'y', label='Sur Z')
+    plt.plot(Ag, Xg, 'r', label='Sur X')
+    plt.plot(Ag, Yg, 'b', label='Sur Y')
+    plt.title('Grotte (seed = ' + str(graine) + ')')
+    plt.legend(loc=0)
+    plt.savefig("Affichage_de_la_map/Grotte1_sur_les_axes ")
+    plt.close()
+    '''
+
+    ax.scatter(Xg, Yg, Zg, s=.8)
+
+    for i in range((u - 1) * alpha + 1):
+        explose(CarteListe3D, int(Xg[i]), int(Zg[i]), int(Yg[i]))
+
+def grotte_3D(ax):
+
+    ax.set_xlabel('X')
+    ax.set_ylabel('Y')
+    ax.set_zlabel('Z')
+    ax.invert_zaxis()
+
+    ax.set_title('Grotte seed=' + str(graine))
+    plt.savefig("Affichage_de_la_map/Grotte")
+
+## Eau
+
+def Eau(CarteListe3D, eau):
+    for e in eau:
+            x, y, z = e
+            CarteListe3D[x][z][y] = 4
+
 def troue(C, sable):
     tot = len(sable)
     ex_sable = []
@@ -174,14 +320,11 @@ def troue(C, sable):
             ex_sable.append(e)
     return ex_sable
 
-stonks = []
-
 def percolation(C, ex_sable, Heau):
     for (x, y, z) in ex_sable:
         C[x][z][y] = 4
     while len(ex_sable) != 0:
         e = ex_sable[0]
-        stonks.append(len(ex_sable))
         x, y, z = e
 
         if z != hauteur - 1 and C[x][z + 1][y] == 0:
@@ -207,150 +350,23 @@ def percolation(C, ex_sable, Heau):
     # plt.plot([i for i in range(len(stonks))],stonks)
     # plt.show()
 
-
-#
-
-
-def Patron_carte(BruitP2D, autre):
-    eau = []
-    sable = []
-    print("Creation tableaux vide")
-    CarteListe3D = [[[1 for y in range(Taille)] for z in range(hauteur)] for x in trange(Taille)]
-    print("fin creation tablaux")
-
-    for x in trange(Taille):
-        for y in range(Taille):
-            Hsurface = int(BruitP2D[x, y])
-            z = 0
-            while z < Hsurface:
-                CarteListe3D[x][z][y] = 0
-                if z >= Heau:
-                    eau.append((x, y, z))
-                z += 1
-            if z < Hneige:
-                CarteListe3D[x][z][y] = 11
-            elif z < Heau - 4:
-                CarteListe3D[x][z][y] = 2
-            else:
-                CarteListe3D[x][z][y] = 5
-                if z >= Heau:
-                    sable.append((x, y, z))
-            z += 1
-            a = 3
-            for i in range(a):
-                if z + i >= Heau - 4:
-                    CarteListe3D[x][z + i][y] = 5
-                else:
-                    CarteListe3D[x][z + i][y] = 3
-            if random.random() < 1 / 3:
-                CarteListe3D[x][hauteur - 3][y] = 10
-            if random.random() < 1 / 2:
-                CarteListe3D[x][hauteur - 2][y] = 10
-            CarteListe3D[x][hauteur - 1][y] = 10
-
-    return CarteListe3D, sable, eau, Heau
-
-
-def minerais(CarteListe3D, ax):
-    h = ((Taille**2)/2500)
-
-    # charbon
-    k = int(15*h)
-    _, Yc = liste_aleatoire(k, Taille - 4, k)
-    _, Xc = liste_aleatoire(k, Taille - 4, k)
-    _, Z = liste_aleatoire(k, hauteur // 2 - 4, k)
-    Zc = [i + hauteur // 2 for i in Z]
-    for i in range(k):
-        charbon(CarteListe3D, int(Xc[i]), int(Zc[i]), int(Yc[i]))
-
-    # redstone
-    k = int(11*h)
-    _, Yr = liste_aleatoire(k, Taille - 4, k)
-    _, Xr = liste_aleatoire(k, Taille - 4, k)
-    _, Z = liste_aleatoire(k, hauteur // 4 - 4, k)
-    Zr = [i + 3*(hauteur // 4) for i in Z]
-    for i in range(k):
-        redstone(CarteListe3D, int(Xr[i]), int(Zr[i]), int(Yr[i]))
-
-    # fer
-    k = int(11*h)
-    _, Yi = liste_aleatoire(k, Taille - 3, k)
-    _, Xi = liste_aleatoire(k, Taille - 3, k)
-    _, Z = liste_aleatoire(k, hauteur // 3 - 3, k)
-    Zi = [i + 2*(hauteur // 3) for i in Z]
-    for i in range(k):
-        iron(CarteListe3D, int(Xi[i]), int(Zi[i]), int(Yi[i]))
-
-    # or
-    k = int(8*h)
-    _, Yo = liste_aleatoire(k, Taille - 2, k)
-    _, Xo = liste_aleatoire(k, Taille - 2, k)
-    _, Z = liste_aleatoire(k, hauteur // 2 - 2, k)
-    Zo = [i + hauteur // 2 for i in Z]
-    for i in range(k):
-        gold(CarteListe3D, int(Xo[i]), int(Zo[i]), int(Yo[i]))
-
-    # diamant
-    k = int(5*h)
-    _, Yd = liste_aleatoire(k, Taille - 2, k)
-    _, Xd = liste_aleatoire(k, Taille - 2, k)
-    _, Z = liste_aleatoire(k, hauteur // 5 - 2, k)
-    Zd = [i + 4 * (hauteur // 5) for i in Z]
-    for i in range(k):
-        diamant(CarteListe3D, int(Xd[i]), int(Zd[i]), int(Yd[i]))
-
-    ax.scatter(Xr, Yr, Zr, c='red', s=0.7)
-    ax.scatter(Xi, Yi, Zi, c='beige', s=0.7)
-    ax.scatter(Xo, Yo, Zo, c='gold', s=0.7)
-    ax.scatter(Xc, Yc, Zc, c='black', s=0.7)
-    ax.scatter(Xd, Yd, Zd, c='aqua', s=0.7)
-
-
-def grotte(CarteListe3D, ax):
-    alpha = 200
-    u = 5
-    _, Xg = liste_aleatoire_spline1(Taille, Taille, u, alpha)
-    _, Zg = liste_aleatoire_spline1(Taille, hauteur, u, alpha)
-    _, Yg = liste_aleatoire_spline1(Taille, Taille, u, alpha)
-
-    '''
-    plt.close("all")
-    plt.plot(Ag, Zg, 'y', label='Sur Z')
-    plt.plot(Ag, Xg, 'r', label='Sur X')
-    plt.plot(Ag, Yg, 'b', label='Sur Y')
-    plt.title('Grotte (seed = ' + str(graine) + ')')
-    plt.legend(loc=0)
-    plt.savefig("Affichage_de_la_map/Grotte1_sur_les_axes ")
-    plt.close()
-    '''
-
-    ax.scatter(Xg, Yg, Zg, linewidths=.2)
-
-    for i in range((u - 1) * alpha + 1):
-        explose(CarteListe3D, int(Xg[i]), int(Zg[i]), int(Yg[i]))
-
-
-def ajout_detail(graine, CarteListe3D, BruitP2D, sable, eau, Heau):
+def ajout_detail(graine, CarteListe3D, sable, eau, Heau):
     plt.close("all")
     ax = plt.axes(projection="3d")
 
     minerais(CarteListe3D, ax)
 
-    # grotte 1
+    # grotte
     for _ in trange(nbGrotte):
         grotte(CarteListe3D, ax)
 
     # eau
-    for e in eau:
-        x, y, z = e
-        CarteListe3D[x][z][y] = 4
+    Eau(N, eau)
     ex_sable = troue(CarteListe3D, sable)
     percolation(CarteListe3D, ex_sable, Heau)
 
     # frames
-    for i in trange(Taille):
-        sauvegarder_grille(CarteListe3D[i], graine, i, f"Affichage_de_la_map/Frame/TIPE{i}.jpg")
-        plt.close()
+    frames()
 
     # grotte 3D
 
@@ -364,14 +380,7 @@ def ajout_detail(graine, CarteListe3D, BruitP2D, sable, eau, Heau):
 
     return CarteListe3D
 
-
-def gif():
-    frames = []
-    for i in trange(Taille):
-        image = imageio.v2.imread(f"Affichage_de_la_map/Frame/TIPE{i}.jpg")
-        frames.append(image)
-    imageio.mimsave(f"Affichage_de_la_map/GIF.gif", frames, duration=7)
-
+## Conversion pour Minecraft
 
 correspondanceID = {0 : 0,  # white = Air
                     1 : 1,  # gray = Stone
@@ -385,9 +394,8 @@ correspondanceID = {0 : 0,  # white = Air
                     9 : 14,  # gold = Gold
                     10: 7,  # noir = Bedrock
                     11: 80,  # = neige
-                    12:71,
+                    12: 15,
                     }
-
 
 def creteMapSchem(grid: list, deltaX: int, deltaY: int, deltaZ: int, graine):
     sf = SchematicFile(shape=(deltaZ, deltaY, deltaX))  # z = hauteur faux mais pas grave
@@ -399,8 +407,10 @@ def creteMapSchem(grid: list, deltaX: int, deltaY: int, deltaZ: int, graine):
     sf.save(f'OutSchem/Carte{graine}.schematic')
     print("Done.")
 
+## Fonction final
 
 def fait_une_map(graine):
+    
     startAll = time.time()
     random.seed(graine)
     np.random.seed(graine)
@@ -409,19 +419,39 @@ def fait_une_map(graine):
 
     start = time.time()
     print("Start Bruit Perlin")
-    M, autre = perlinfzej(graine, Taille, hauteur)
-
+    M  = perlinfzej(graine, Taille, hauteur)
     print(f"End Bruit Perlin TimeToFinish: {time.time() - start:.2f} s")
 
     start = time.time()
     print("Start Patron")
-    N, sable, eau, Heau = Patron_carte(M, autre)
+    Map, sable, eau = Patron_carte(M)
     print(f"End Patron TimeToFinish: {time.time() - start:.2f} s")
 
     start = time.time()
-    print("Start Detail")
-    finished = ajout_detail(graine, N, M, sable, eau, Heau)
-    print(f"End Detail TimeToFinish: {time.time() - start:.2f} s")
+    print("Start Minerai")
+    plt.close("all")
+    ax = plt.axes(projection="3d")
+    minerais(Map, ax)
+    print(f"End Minerai TimeToFinish: {time.time() - start:.2f} s")
+
+    start = time.time()
+    print("Start Grotte")
+    for _ in trange(nbGrotte):
+        grotte(Map, ax)
+    grotte_3D(ax)
+    print(f"End Grotte TimeToFinish: {time.time() - start:.2f} s")
+
+    start = time.time()
+    print("Start Eau")
+    Eau(Map, eau)
+    ex_sable = troue(Map, sable)
+    percolation(Map, ex_sable, Heau)
+    print(f"End Eau TimeToFinish: {time.time() - start:.2f} s")
+
+    start = time.time()
+    print("Start Frame")
+    frames(Map)
+    print(f"End Frame TimeToFinish: {time.time() - start:.2f} s")
 
     start = time.time()
     print("Start Gif")
@@ -430,23 +460,22 @@ def fait_une_map(graine):
 
     start = time.time()
     print("Start Schematic")
-    creteMapSchem(finished, Taille, Taille, hauteur, graine)
+    creteMapSchem(Map, Taille, Taille, hauteur, graine)
     print(f"End Schematic TimeToFinish: {time.time() - start:.2f} s")
 
     finTime = time.time() - startAll
     plt.show()
     print(f"Program Finished in {finTime:.2f} s")
 
-
-##
-## Modélisation de la carte
+## Paramètres de la carte
+alpha = 400
 Hneige = 60
 Heau = 150
-nbGrotte = 8
-Taille = 200
+nbGrotte = 10
+Taille = 100
 hauteur = 256
 graine = randrange(10000)
-fait_une_map(graine)
 
-# prendre un bruit de perlin en basse resolution et faire **5
-# ajouter un bruit de perlin tres leger max 3 bloc entre min et max
+## Création de la carte
+
+fait_une_map(graine)
