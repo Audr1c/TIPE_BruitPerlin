cc2a0210079a08744b990c46f8028ee85d3d0411
Pride30 <eliott@famillefavier.fr>
2023-05-25 13:42:19 +0200

    Bonjour monsieur

--- Map.py
+++ Map.py
@@ -52,7 +52,7 @@ def Patron_carte(BruitP2D):
 ## Images
 
 def sauvegarder_grille(grille: list, g, i, nom_de_fichier: str) -> None:
-    echelle = ListedColormap(['#d7edf8', 'gray', '#007412', 'brown', 'blue', '#e9f05d', 'black', 'red', '#00fdbd', 'gold', '0.3', 'white','#c9d279','#095a03'], 13)
+    echelle = ListedColormap(['#d7edf8', 'gray', '#007412', 'brown', 'blue', 'yellow', 'black', 'red', '#00fdbd', 'gold', '0.3', 'white','#c9d279','#095a03'], 13)
     plt.matshow(grille, cmap=echelle, vmin=0, vmax=13)
     plt.title(f"x={i} g={g}")  # 'x=' + str(i) + ' g=' + str(g)
     plt.xlabel('y')
@@ -361,6 +361,9 @@ def Pantation(C,L_arbre,BdP):
         z=int(BdP[x,y])
         if z<Heau-3:
                 C[x][z-1][y]=13
+
+def Ecosia(tkt):
+    return tkt
 
 ## Conversion pour Minecraft
 
